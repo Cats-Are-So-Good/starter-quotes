8c8ee6e0db8ab216718b11b9c4ecf0d9bde2168a
erick-fda <efernandezcraft@gmail.com>
2016-01-28 17:34:55 -0800

    Fixed image link 1 Signed-off-by:erick-fda <efernandezcraft@gmail.com>

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -14,10 +14,9 @@ class First extends Application {
 		parent::__construct();
 	}
 
-	//-------------------------------------------------------------
-	//  The normal pages
-	//-------------------------------------------------------------
-
+	/**
+         * Default method for showing the first quote in our mock database.
+         */
 	function index()
 	{
             // Get the single quote view.
@@ -34,6 +33,26 @@ class First extends Application {
             // Render the view.
             $this->render();
 	}
+        
+        /**
+         * Show Bob Monkhouse's quote.
+         */
+        function zzz()
+        {
+            // Get the single quote view.
+            $this->data['pagebody'] = 'justone';
+            
+            // Get Monkhouse's record from the quotes object.
+            $record = $this->quotes->get('1');
+            
+            // Set the values of the view parameters.
+            $this->data['who'] = $record['who'];
+            $this->data['what'] = $record['what'];
+            $this->data['mug'] = $record['mug'];
+            
+            // Render the view.
+            $this->render();
+        }
 }
 
 /* End of file First.php */
